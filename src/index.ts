dbb52ed8dcf557d92c865d146242dc67777d50b7
Hezekiah Hendry <46537907+RealShadowNova@users.noreply.github.com>
2022-04-13 22:09:49 -0400

    chore: cleanup and nitpick (#1)
    * chore(build): remove dist from git tracking
    
    * chore: sort package.json
    
    * chore: out of date yarn lock
    
    * chore(tsconfig): rewrite config for extra nice strictness
    
    * chore(src): little cleanup and nitpick

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,75 @@
-const defers = new Map();
+const defers = new Map<string, Defer>();
+
+interface Defer {
+  promise: Promise<unknown>;
+
+  resolve(value: unknown): void;
+
+  reject(reason: string): void;
+}
 
 /**
- * Create a new deferral promise, saves it, and returns it.
- * @param id The name/identifier of the deferral. If it exists, it will be overwritten.
- * @returns The promise itself, which will be resolved when `resolveDefer(id)` is called.
+ * Create a new deferral promise, saves it and returns it.
+ * @since 1.0.0
+ * @param key The identifier of the deferral. If it exists, it will be overwritten.
+ * @returns The promise itself, which will be resolved when {@link resolveDefer} is called.
  */
-export const makeDefer = (id: string): Promise<unknown> => {
-  let resolve;
-  let reject;
+export function makeDefer(key: string): Promise<unknown> {
+  let resolve: (value: unknown) => void;
+  let reject: (reason?: any) => void;
+
   const promise = new Promise((res, rej) => {
     resolve = res;
     reject = rej;
   });
-  defers.set(id, {
-    promise,
-    resolve,
-    reject,
-  });
+
+  // @ts-expect-error 2454
+  defers.set(key, { promise, resolve, reject });
+
   return promise;
-};
+}
 
 /**
  * Gets a deferral, to wait for it to be resolved.
- * @param id The name/identifier of the deferral. 
- * @returns The promise to await/resolve. If it does not exist, returns null.
+ * @since 1.0.0
+ * @param key The identifier of the deferral.
+ * @returns The promise to resolve. If it does not exist, returns null.
  */
-export const waitForDefer = (id: string): Promise<unknown>|null => {
-  return defers.get(id).promise;
-};
+export function waitForDefer(key: string): Promise<unknown> | null {
+  return defers.has(key) ? defers.get(key)!.promise : null;
+}
 
 /**
- * Get an array of currently active deferral IDs
- * @returns An array of string IDs currently active in Deferrals
+ * Get an array of currently active deferrals by key.
+ * @since 1.0.0
+ * @returns An array of string keys currently active in deferrals.
  */
-export const listDefers = ():string[] => Array.from(defers.keys());
+export function listDefers(): string[] {
+  return Array.from(defers.keys());
+}
 
 /**
  * Resolves a deferral, which will resolve any reference to it in your app.
- * @param id The name/identifier of the deferral to resolve.
+ * @since 1.0.0
+ * @param key The identifier of the deferral to resolve.
  * @param value The value given to all resolved deferrals in your app.
  */
-export const resolveDefer = (id: string, value: any): void => {
-  defers.get(id).resolve(value);
-  defers.delete(id);
-};
+export function resolveDefer(key: string, value: unknown): void {
+  if (defers.has(key)) {
+    defers.get(key)!.resolve(value);
+    defers.delete(key);
+  }
+}
 
 /**
  * Rejects a deferral, which will reject any reference to it in your app.
- * If not caught, these rejections can cause erors to be thrown in your code!
- * @param id The name/identifier of the deferral to reject.
- * @param error The error string given to your rejected deferrals
+ * @since 1.0.0
+ * @param key The identifier of the deferral to reject.
+ * @param reason The reason given to your rejected deferrals
  */
-export const rejectDefer = (id: string, error: string): void => {
-  defers.get(id).reject(error);
-  defers.delete(id);
-};
+export function rejectDefer(key: string, reason?: any): void {
+  if (defers.has(key)) {
+    defers.get(key)!.reject(reason);
+    defers.delete(key);
+  }
+}
